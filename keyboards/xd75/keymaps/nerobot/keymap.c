6863f894a24f72e919ae9315c4f4efa4705c574b
Steven Daglish <s.c.daglish@gmail.com>
2019-09-23 13:05:07 +0100

    Update keymap.c

--- keyboards/xd75/keymaps/nerobot/keymap.c
+++ keyboards/xd75/keymaps/nerobot/keymap.c
@@ -38,21 +38,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
  * | CAP LK | '      | q      | j      | k      | x      | {      |        | }      | b      | m      | w      | v      | z      | ENTER  |
  * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+--------+-----------------+--------+--------|
- * | LCTRL  | Fn     | Win    | LAlt   | SPACE  | Fn     | \      |        | /      | Fn     | SPACE  |        |        |        | RCTRL  |
+ * | LCTRL  | Fn     | Win    | LAlt   | BACKSP | Fn     | \      |        | /      | Fn     | SPACE  |        |        |        | RCTRL  |
  * '--------------------------------------------------------------------------------------------------------------------------------------'
  */
 
   [_QW] = LAYOUT_ortho_5x15( /* QWERTY */
     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_LABK, KC_EQL,  KC_RABK, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
     KC_TAB,  KC_SCLN, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_LBRC, KC_BSLS, KC_RBRC, KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_MINS,
-    KC_CAPS, KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_LPRN, KC_DEL,  KC_RPRN, KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_ENT,
-    KC_LSFT, KC_QUOT, KC_Q,    KC_J,    KC_K,    KC_X,    KC_LCBR, KC_UP,   KC_RCBR, KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_RSFT,
-    KC_LCTL, MO(_FN), KC_LWIN, KC_LALT, KC_SPC,  MO(_FN), KC_BSLS, KC_DOWN, KC_SLSH, MO(_FN), KC_SPC,  MO(_FN), KC_RALT, KC_RGUI, KC_RCTL
+    KC_LSFT, KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_LPRN, KC_DEL,  KC_RPRN, KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_RSFT,
+    KC_CAPS, KC_QUOT, KC_Q,    KC_J,    KC_K,    KC_X,    KC_LCBR, KC_UP,   KC_RCBR, KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_ENT,
+    KC_LCTL, MO(_FN), KC_LWIN, KC_LALT, KC_BSPC, MO(_FN), KC_BSLS, KC_DOWN, KC_SLSH, MO(_FN), KC_SPC,  MO(_FN), KC_RALT, KC_RGUI, KC_RCTL
   ),
 
 /* FUNCTION
  * .--------------------------------------------------------------------------------------------------------------------------------------.
- * | F1     | F2     | F3     | F4     | F5     | F6     | NUM LK | P/     | P*     | F7     | F8     | F9     | F10    | F11    | RESET  |
+ * | F1     | F1     | F2     | F3     | F4     | F5     | NUM LK | P/     | P*     | F6     | F7     | F8     | F9     | F10    | RESET  |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
  * | SELECT | CALC   | MYCOMP | MAIL   | RGB HD | RGB HI | P7     | P8     | P9     | -      |        |        | PR SCR | SCR LK | PAUSE  |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
@@ -65,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
   [_FN] = LAYOUT_ortho_5x15( /* FUNCTION */
-    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_NLCK, KC_SLSH, KC_ASTR, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  RESET,
+    KC_F1,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_NLCK, KC_SLSH, KC_ASTR, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  RESET,
     KC_MSEL, KC_CALC, KC_MYCM, KC_MAIL, RGB_HUD, RGB_HUI, KC_P7,   KC_P8,   KC_P9,   KC_MINS, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS,
     KC_MPRV, KC_MPLY, KC_MNXT, KC_MSTP, RGB_SAD, RGB_SAI, KC_P4,   KC_P5,   KC_P6,   KC_PLUS, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, _______,
     KC_VOLD, KC_MUTE, KC_VOLU, KC_APP,  RGB_VAD, RGB_VAI, KC_P1,   KC_P2,   KC_P3,   KC_PENT, _______, _______, _______, _______, _______,
