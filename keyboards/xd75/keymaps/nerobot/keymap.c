a77cde4a9b7d78f5c35613913e98368231352896
Steven Daglish <s.c.daglish@gmail.com>
2019-10-28 08:40:46 +0000

    Update keymap.c

--- keyboards/xd75/keymaps/nerobot/keymap.c
+++ keyboards/xd75/keymaps/nerobot/keymap.c
@@ -17,8 +17,8 @@
 
 // Layer shorthand
 #define _QW 0
-#define _FN 1
-#define _L1 2  		// Currently not used
+#define _RAISE 1
+#define _LOWER 2
 
 // Defines the keycodes used by our macros in process_record_user
 enum custom_keycodes {
@@ -43,71 +43,31 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
- /* QWERTY
- * .--------------------------------------------------------------------------------------------------------------------------------------.
- * | ESC             | 1      | 2      | 3      | 4      | 5      | <      | =+     | >      | 6      		| 7      		| 8      | 9      | 0      |                |
- * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------|
- * | TAB             | '      | ,      | .      | p      | y      | [ {    |        | ] }    | f      		| g      		| c      | r      | l      | _             |
- * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------|
- * | TD(TD_SFT_CAP)  | a      | o      | e      | u      | i      | (      |        | )      | d      		| h      		| t      | n      | s      | TD(TD_SFT_CAP) |
- * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
- * | LCTRL / ESC     | ;      | q      | j      | k      | x      | {      |        | }      | b      		| m      		| w      | v      | z      | ENTER          |
- * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+--------+-----------------+--------+--------|
- * | LCTRL           | Fn     | Win    | LAlt   | BACKSP | DEL    | \      |        | /      | _        	| Fn / SPACE	|        |        |        | RCTRL          |
- * '--------------------------------------------------------------------------------------------------------------------------------------'
- */
-
   [_QW] = LAYOUT_ortho_5x15( /* QWERTY */
-    KC_ESC, 		KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_LABK, 			KC_EQL,  KC_RABK, KC_6,    			KC_7,    	KC_8,    KC_9,    KC_0,    KC_DEL,
-    KC_TAB,  		KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_LBRC, 			KC_BSLS, KC_RBRC, KC_F,    			KC_G,   	KC_C,    KC_R,    KC_L,    KC_MINS,
-    KC_LSFT,	KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_LPRN, 			KC_DEL,  KC_RPRN, KC_D,    			KC_H,    	KC_T,    KC_N,    KC_S,    KC_RSFT,
-    CTL_T(KC_ESC),	KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_LCBR, 			KC_UP,   KC_RCBR, KC_B,    			KC_M,   	KC_W,    KC_V,    KC_Z,    CTL_T(KC_ENT),
-    KC_LCTL, 		MO(_FN), KC_LWIN, KC_LALT, KC_BSPC, KC_DEL , KC_NONUS_BSLASH, 	KC_DOWN, KC_SLSH, LT(_FN,KC_SPC),	KC_UNDS,	MO(_FN), KC_RALT, KC_RGUI, KC_RCTL
+    KC_ESC, 		KC_1,    	KC_2,    	KC_3,    	KC_4,    		KC_5,    	KC_1,		KC_EQL,		KC_RABK,	KC_6,    	KC_7,    	KC_8,    	KC_9,    	KC_0,    	KC_DEL,
+    KC_TAB,  		KC_QUOT, 	KC_COMM, 	KC_DOT,  	KC_P,    		KC_Y,    	KC_7,		KC_8, 		KC_9, 		KC_F,    	KC_G,   	KC_C,    	KC_R,    	KC_L,    	KC_MINS,
+    KC_LSFT,		KC_A,    	KC_O,    	KC_E,    	KC_U,    		KC_I,    	KC_4,		KC_5,  		KC_6, 		KC_D,    	KC_H,    	KC_T,    	KC_N,    	KC_S,    	KC_RSFT,
+    CTL_T(KC_ESC),	KC_SCLN, 	KC_Q,    	KC_J,    	KC_K,    		KC_X,    	KC_1,		KC_2,  		KC_3, 		KC_B,    	KC_M,   	KC_W,    	KC_V,    	KC_Z,    	CTL_T(KC_ENT),
+    KC_LCTL, 		_______ , 	KC_LWIN, 	KC_LALT, 	MO(_RAISE),		KC_BSPACE, 	KC_0,		KC_0,		KC_DOT, 	KC_SPC,		MO(_LOWER),	KC_UNDS, 	KC_RALT, 	KC_RGUI, 	KC_RCTL
   ),
 
-/* FUNCTION
- * .--------------------------------------------------------------------------------------------------------------------------------------.
- * | F1     | F1     | F2     | F3     | F4     | F5     | NUM LK | P/     | P*     | F6     | F7     | F8     | F9     | F10    | RESET  |
- * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * | SELECT | [      | [      | ]      | RGB HD | RGB HI | P7     | P8     | P9     | -      |        |        | PR SCR | SCR LK | PAUSE  |
- * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * | PREV   | {      | {      | }      | =      | RGB SI | P4     | P5     | P6     | +      | LEFT   | DOWN   | UP     | RIGHT  |        |
- * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * | VOL-   | MUTE   | VOL+   | APP    | RGB VD | RGB VI | P1     | P2     | P3     | PENT   | KC_NUHS|        |        |        |        |
- * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * |        |        | RGB TG | FN     | RGB RMD| RGB MD |        |  P0    | P.     | PENT   | PENT   | FN     |        |        |        |
- * '--------------------------------------------------------------------------------------------------------------------------------------'
- */
 
-  [_FN] = LAYOUT_ortho_5x15( /* FUNCTION */
-    KC_F1,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_NLCK, KC_SLSH, KC_ASTR, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  RESET,
-    KC_MSEL, KC_LBRC, KC_LBRC, KC_RBRC, RGB_HUD, RGB_HUI, KC_P7,   KC_P8,   KC_P9,   KC_MINS, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS,
-    KC_MPRV, KC_LCBR, KC_LCBR, KC_RCBR, KC_EQL,  RGB_SAI, KC_P4,   KC_P5,   KC_P6,   KC_PLUS, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, _______,
-    KC_VOLD, KC_MUTE, KC_VOLU, KC_APP,  RGB_VAD, RGB_VAI, KC_P1,   KC_P2,   KC_P3,   KC_PENT, KC_NUHS, _______, _______, _______, _______,
-    _______, _______, _______, RGB_TOG, RGB_RMOD,RGB_MOD, _______, KC_P0,   KC_PDOT, KC_PENT, KC_PENT, MO(_FN), _______, _______, _______
+  [_LOWER] = LAYOUT_ortho_5x15( /* FUNCTION */
+    _______,   		KC_F1,   	KC_F2,   	KC_F3,   	KC_F4,   		KC_F5,   	_______, 	_______, 	_______, 	KC_F6,   	KC_F7,   	KC_F8,   	KC_F9,   	KC_F10,  	RESET,
+    _______,		KC_1,    	KC_2,    	KC_3,    	KC_4,			KC_5,    	_______,	_______,	_______,	KC_6, 		KC_7,  		KC_8,    	KC_9,    	KC_0,    	_______,
+    KC_CAPS, 		KC_NUBS, 	KC_LCBR, 	KC_RCBR, 	KC_EQL,  		KC_SLSH, 	_______,   	_______,   	_______,   	_______, 	KC_HOME, 	KC_PGDOWN, 	KC_PGUP, 	KC_END, 	_______,
+    _______,   		_______,   	KC_LBRC,   	KC_RBRC,   	KC_UNDS,   		KC_MINS,   	_______, 	_______, 	_______, 	_______,   	_______,   	_______,   	_______,   	_______,  	_______,
+    _______, 		_______, 	_______, 	_______, 	_______,		KC_DEL, 	_______, 	_______,   	_______, 	_______, 	_______, 	_______, 	_______, 	_______, 	_______
   ),
 
-      /* MOVEMENT LAYER
- * .--------------------------------------------------------------------------------------------------------------------------------------.
- * | F1     | F2     | F3     | F4     | F5     | F6     | NUM LK | P/     | P*     | F7     | F8     | F9     | F10    | F11    | F12    |
- * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * | SELECT | CALC   | MYCOMP | MAIL   | RGB HD | RGB HI | P7     | P8     | P9     | -      |        |        | PR SCR | SCR LK | PAUSE  |
- * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * | PREV   | PLAY   | NEXT   | STOP   | RGB SD | RGB SI | P4     | P5     | P6     | +      | LEFT   | DOWN   | UP     | RIGHT  |        |
- * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * | VOL-   | MUTE   | VOL+   | APP    | RGB VD | RGB VI | P1     | P2     | P3     | PENT   |        |        |        |        |        |
- * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * |        |        | RGB TG | FN     | RGB RMD| LUp    |        | P0     | P.     | LDown  | PENT   | FN     |        |        |        |
- * '--------------------------------------------------------------------------------------------------------------------------------------'
- */
 
-  [_L1] = LAYOUT_ortho_5x15( /* FUNCTION */
-    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_NLCK, KC_SLSH, KC_ASTR, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-    KC_MSEL, KC_CALC, KC_MYCM, KC_MAIL, RGB_HUD, RGB_HUI, KC_P7,   KC_P8,   KC_P9,   KC_MINS, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS,
-    KC_MPRV, KC_MPLY, KC_MNXT, KC_MSTP, RGB_SAD, RGB_SAI, KC_P4,   KC_P5,   KC_P6,   KC_PLUS, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, _______,
-    KC_VOLD, KC_MUTE, KC_VOLU, KC_APP,  RGB_VAD, RGB_VAI, KC_P1,   KC_P2,   KC_P3,   KC_PENT, _______, _______, _______, _______, _______,
-    _______, _______, _______, RGB_TOG, RGB_RMOD,TG(_L1), _______, KC_P0,   KC_PDOT, TG(_L1), KC_PENT, MO(_FN), _______, _______, _______
-  )
+  [_RAISE] = LAYOUT_ortho_5x15( /* FUNCTION */
+    KC_F1,   		KC_F1,   	KC_F2,   	KC_F3,   	KC_F4,   		KC_F5,   	_______, 	_______, 	_______, 	KC_F6,   	KC_F7,   	KC_F8,   	KC_F9,   	KC_F10,  	RESET,
+    KC_ESC,			LSFT(KC_1),	LSFT(KC_2), LSFT(KC_3),	LSFT(KC_4),		LSFT(KC_5),	_______,	_______,	_______,	LSFT(KC_6),	LSFT(KC_7),	LSFT(KC_8),	LSFT(KC_9),	LSFT(KC_0),	KC_DEL,
+    KC_MPRV, 		KC_PIPE, 	_______, 	_______, 	KC_PPLS,  		KC_QUES, 	_______,   	_______,   	_______,   	KC_PLUS, 	KC_LEFT, 	KC_DOWN, 	KC_UP  , 	KC_RGHT, 	_______,
+    KC_F1,   		KC_F1,   	KC_F2,   	KC_F3,   	KC_F4,   		KC_F5,   	_______, 	_______, 	_______, 	KC_F6,   	KC_F7,   	KC_F8,   	KC_F9,   	KC_F10,  	RESET,
+    _______, 		_______, 	_______, 	_______, 	_______,		KC_DEL, 	_______, 	_______,   	_______, 	KC_PENT, 	KC_PENT, 	_______, 	_______, 	_______, 	_______
+  ),	
 
 };
 
