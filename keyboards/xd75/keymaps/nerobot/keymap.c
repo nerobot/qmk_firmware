9d8d6408f6ee30cd61940bf6ebf31980f3c00685
Steven Daglish <s.c.daglish@gmail.com>
2019-11-12 07:02:26 +0000

    keycode update

--- keyboards/xd75/keymaps/nerobot/keymap.c
+++ keyboards/xd75/keymaps/nerobot/keymap.c
@@ -56,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,   		KC_F1,   	KC_F2,   	KC_F3,   	KC_F4,   		KC_F5,   	_______, 	_______, 	_______, 	KC_F6,   	KC_F7,   	KC_F8,   	KC_F9,   	KC_F10,  	RESET,
     _______,		KC_1,    	KC_2,    	KC_3,    	KC_4,			KC_5,    	_______,	_______,	_______,	KC_6, 		KC_7,  		KC_8,    	KC_9,    	KC_0,    	_______,
     KC_CAPS, 		KC_NUBS, 	KC_LCBR, 	KC_RCBR, 	KC_EQL,  		KC_SLSH, 	_______,   	_______,   	_______,   	_______, 	KC_HOME, 	KC_PGDOWN, 	KC_PGUP, 	KC_END, 	_______,
-    _______,   		_______,   	KC_LBRC,   	KC_RBRC,   	KC_UNDS,   		KC_MINS,   	_______, 	_______, 	_______, 	_______,   	_______,   	_______,   	_______,   	_______,  	_______,
+    KC_CAPS, 		KC_NUBS, 	KC_LCBR, 	KC_RCBR, 	KC_EQL,  		KC_SLSH, 	_______,   	_______,   	_______,   	_______, 	KC_HOME, 	KC_PGDOWN, 	KC_PGUP, 	KC_END, 	_______,
     _______, 		_______, 	_______, 	_______, 	_______,		KC_DEL, 	_______, 	_______,   	_______, 	_______, 	_______, 	_______, 	_______, 	_______, 	_______
   ),
 
